83db30c5382dcfa75c202d7c85405d5f0269084e
yzj <darkblue.young@foxmail.com>
2016-03-20 13:58:44 +0800

    add scc

--- Cpp1.cpp
+++ Cpp1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string.h>
 int main()
 {
 	int a=5,b=3;
@@ -6,7 +7,10 @@ int main()
 	b=a^b;
 	a=a^b;
 	std::cout<<a<<b;
-	cout<<a;
-	cout<<"hhhh";
+	char str[]="hello world";
+	strcpy(str,"hf");
+	printf("%s\n",str);
+	printf("%c\n",str[1]);
+	printf("%c\n",str[2]);
 	return 0;
 }
