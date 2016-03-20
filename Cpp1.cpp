1929b4390ee0afa5d50bbec9f0f7ea4bd2167218
yzj <darkblue.young@foxmail.com>
2016-03-20 15:45:48 +0800

    friend

--- Cpp1.cpp
+++ Cpp1.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
+using namespace std;
 #include<string.h>
+class Tt
+{
+public:
+	Tt();
+
+private:
+
+	static int a;
+	int b;
+	friend void pp(Tt ttt);
+};
+Tt::Tt()
+{
+	b=444;
+}
+void pp(Tt ttt)
+{
+	cout<<ttt.b;
+}
+int Tt::a = 3333;
 int main()
 {
 	int a=5,b=3;
@@ -17,5 +38,7 @@ int main()
 	//char d[]="123";
 	strcpy(d,s);
 	printf("%s\n%s\n",d,s);
+	Tt tt;
+	pp(tt);
 	return 0;
 }
